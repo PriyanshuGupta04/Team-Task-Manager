2ccb619a194c2db0d10f54ccfbc9d67fcc058b37
Priyanshu Gupta <priyanshugupta@gmail.com>
2026-04-30 21:47:55 +0530

    remove projects route

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/tasks", require("./routes/tasks"));
-app.use("/api/projects", require("./routes/projects"));
+//app.use("/api/projects", require("./routes/projects"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
